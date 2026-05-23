1ac8240275b0588ca362726cbc6842d7d4ffc30d
Roland Bbosa <71003077+rolandbbosa@users.noreply.github.com>
2026-05-23 10:52:19 +0300

    Update tubevideos.js

--- js/tubevideos.js
+++ js/tubevideos.js
@@ -141,7 +141,7 @@ function getDailySeed() {
 }
 
 function buildHash(page = null, videoId = null) {
-    const parts = ['#imageporn-TubeVideos'];
+    const parts = ['#TubeVideos'];
     if (page && page > 1) parts.push(`page-${page}`);
     if (videoId) parts.push(`video-${videoId}`);
     return parts.join('/');
